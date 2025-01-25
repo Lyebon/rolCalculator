89ece2a86a01deb1a0f18d936e4b5d00dfa12259
Lyebon <sebas25garcia@gmail.com>
2025-01-25 22:49:06 +0000

    diccionario

--- characters.py
+++ characters.py
@@ -3,26 +3,6 @@ class Characters:
         self.party = party
         self.npc = npc
         self.animals = animals
-        self.menu()
-
-
-    def menu():
-        print("1- PJ")
-        print("2- NPC")
-        print("3- Animal")
-        print("4- Return")
-        menu_num = int(input("Que tipo de personaje quiere crear"))
-
-        if menu_num == 1:
-            Player()
-        elif menu_num == 2:
-            NPC()
-        elif menu_num == 3:
-            Animals()
-        elif menu_num == 4:
-            exit
-        else:
-            raise Exception ("El numero ingresado no pertenece a las opciones")
 
 class Player(Characters):
     def __init__(self, party):
--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-from characters import Characters
-from damage import Calculator
+from characters import *
+from damage import *
+import json
 
-party = []
-npc = []
-animals = []
-
-
+with open('players.json') as f:
+    player = f.read()
 
 def main():
     menu()
 
 def menu():
-    print("1- Creador de Personajes")
-    print("2- Combate")
-    print("3- Salir")
-    menu_num = int(input("Que opcion desea realizar "))
-
-    if menu_num == 1:
-        Characters(party, npc, animals)
-    elif menu_num == 2:
-        Calculator()
-    elif menu_num == 3:
-        exit
-    else:
-        raise Exception ("El numero ingresado no pertenece a las opciones")
+    pass
 
 main()
