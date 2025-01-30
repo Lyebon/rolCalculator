6da241d3f8ed359b707d7fc3a5c1704098949821
Lyebon <sebas25garcia@gmail.com>
2025-01-30 19:00:08 +0000

    restart

--- damage.py
+++ damage.py
@@ -1,3 +1,14 @@
 class Calculator:
-    def __init__(self):
-        pass
+    def __init__(self, tirada, lista):
+        self.tirada = tirada
+        self.lista = lista
+    
+    def pifia(self, tirada, arma):
+        if tirada <= arma:
+            return ('Pifia de arma')
+
+    def result(self, tirada, lista):
+        for num in lista:
+            if tirada <= num:
+                return num
+        return ValueError("Numero Invalido")
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from characters import *
 from damage import *
-import json
 
-with open('players.json') as f:
-    player = f.read()
+tabla_daños = [40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150]
+
 
 def main():
     menu()
 
 def menu():
-    pass
+    while True:
+        tirada = int(input("Ingrese la tirada"))
+        resultado = Calculator(tirada, tabla_daños)
+        print (resultado)
+        
+
+
 
 main()
